f91e3dfbbeab7231da8eba9bb628d8e95ad7d4b4
Tyler Fuqua <tyler@agilityda.com>
2018-01-05 15:56:48 -0700

    Stretch Goal 1

--- IntroJavaHandsOn/src/com/tylerfuqua/handson/HandsOn.java
+++ IntroJavaHandsOn/src/com/tylerfuqua/handson/HandsOn.java
@@ -5,12 +5,17 @@ public class HandsOn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean isSunny = true;
+		boolean atBeach = true;
 		
 		if (isSunny) {
 			System.out.println("wear sunglasses");
 		} else {
-			System.out.println("don't wear sunglasses");
-		}
+			System.out.println("Don't need to wear sunglasses.");	
+		} if (atBeach) {
+			System.out.println("wear sunblock");
+		} else {
+			System.out.println("don't need sunblock");
+		} 
 	}
 
 }
