611fe7fc4816fa6595fd043508a09aeadc09f789
Tyler Fuqua <tyler@agilityda.com>
2018-01-05 15:27:35 -0700

    Hands On Part1

---
+++ IntroJavaHandsOn/src/com/tylerfuqua/handson/HandsOn.java
@@ -0,0 +1,16 @@
+package com.tylerfuqua.handson;
+
+public class HandsOn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isSunny = true;
+		
+		if (isSunny) {
+			System.out.println("wear sunglasses");
+		} else {
+			System.out.println("don't wear sunglasses");
+		}
+	}
+
+}
